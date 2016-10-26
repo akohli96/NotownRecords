d9e1b16aae39fcc0650d261be9960826320c59a5
Ayush Kohli <ayushk96@hotmail.com>
2016-10-26 11:53:14 -0500

    Working through requirements

--- notown/notownapp/models.py
+++ notown/notownapp/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Musicians(models.Model):
+    name = models.CharField(max_length=30)
+    ssn = models.CharField(max_length=10, primary_key=True)
+
+class Instruments(models.Model):
+    instrld = models.CharField(max_length=10,primary_key=True)
+    dname = models.CharField(max_length=30)
+    key = models.CharField(max_length=5)
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+#Using through table
+class Membership (models.Model):
+    ssn = models.ForeignKey(Musicians)
+    instrld = models.ForeignKey(Instruments)
+
+    class Meta:
+        unique_together = ('ssn','instrld')
+
+class SongAppears(models.Model):
+    songID = models.IntegerField()
+
+class Telephone_Home(models.Model):
+    phone = models.CharField(max_length=11,primary_key=True)
+    address = models.ForeignKey('Places') #since not defined yet
+
+
+class Places(models.Model):
+    address = models.CharField(max_length=30,primary_key=True)
+    #address = models.ForeignKey(Telephone_Home)
+
+#class Lives(models.Model):
