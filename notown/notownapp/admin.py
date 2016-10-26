030390928ea29060c484e9f0d96b1d044ee4a8f6
Ayush Kohli <ayushk96@hotmail.com>
2016-10-26 11:52:51 -0500

    all models registered in admin

--- notown/notownapp/admin.py
+++ notown/notownapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.apps import apps
+#from models import *
+
+app = apps.get_app_config('notownapp')
+for model_name,model in app.models.items():
+    admin.site.register(model)
+print "HELLO from admin.py"
