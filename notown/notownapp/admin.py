1f3d4eebaf4f961cba1556a19954c5876b485389
Ayush Kohli <ayushk96@hotmail.com>
2016-10-29 01:41:53 -0500

    Figured out displaying all

--- notown/notownapp/admin.py
+++ notown/notownapp/admin.py
@@ -12,8 +12,8 @@ print "HELLO from admin.py"
 
 
 class MembershipInline(admin.TabularInline):
-    model=Membership
-    extra = 2
+    model=Plays
+    extra = 1
 
 class InstrumentsAdmin(admin.ModelAdmin):
     inlines = (MembershipInline,)
@@ -27,3 +27,9 @@ admin.site.register(Instruments, InstrumentsAdmin)
 app = apps.get_app_config('notownapp')
 for model_name,model in app.models.items():
     print model,model_name
+    if ('musicians' not in model_name and 'instruments' not in model_name):
+        print model
+        #print "NOT THE ONES REGISTERED ALREADY"
+        admin.site.register(model)
+        print "REGISTERED " + str(model)
+        #admin.site.register(model)
