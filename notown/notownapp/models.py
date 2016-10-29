aadf62ad9d772bf0952e959dae6f52f93a7eae0d
Ayush Kohli <ayushk96@hotmail.com>
2016-10-29 18:45:46 -0500

    Added custom rule. Testing integrity

--- notown/notownapp/models.py
+++ notown/notownapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
 
 
 class Musicians(models.Model):
@@ -25,6 +25,7 @@ class Plays (models.Model):
 
     class Meta:
         unique_together = ('ssn','instrld')
+
     def __str__(self):
         return str(self.ssn) + " " + str(self.instrld)
 
@@ -36,16 +37,27 @@ class Album_Producer(models.Model):
     speed = models.IntegerField()
     title = models.CharField(max_length=30)
 
+    def __str__(self):
+        return str(self.albumident) + " " + str(self.ssn) +" " +  str(self.copyright) + " " + str(self.speed) + " " + str(self.title)
+
 """
 This has to be searched by customers. Input can be title,author or the Album_Producer title
 
 """
+
+#Filter can also be applied
+#SongAppears.objects.get(title__contains="Song1")
+#SongAppears.objects.get(author__contains="Ayush")
+#SongAppears.objects.get(albumident__title="Ayush Album1")
 class SongAppears(models.Model):
     songID = models.IntegerField(primary_key=True)
     author = models.CharField(max_length=30)
     title = models.CharField(max_length=30)
     albumident = models.ForeignKey(Album_Producer)
     performs = models.ManyToManyField(Musicians,through="Performs")
+
+    def __str__(self):
+        return str(self.songID) + " \n " + str(self.author) + " \n " + str(self.title) + " \n " + str(self.albumident) + " \n " + str(self.performs)
 
 class Performs(models.Model):
     ssn = models.ForeignKey(Musicians)
@@ -54,13 +66,22 @@ class Performs(models.Model):
     class Meta:
         unique_together=('ssn','songID')
 
+    def __str__(self):
+        return str(self.ssn) + " " + str(self.songID)
+
 
 class Places(models.Model):
     address = models.CharField(max_length=30,primary_key=True)
 
+    def __str__(self):
+        return str(self.address)
+
 class Telephone_Home(models.Model):
     phone = models.CharField(max_length=11,primary_key=True)
     address = models.OneToOneField(Places) #should take care of unique
+
+    def __str__(self):
+        return str(self.phone) + " " + str(self.address)
 
 class Lives(models.Model):
     ssn = models.ForeignKey(Musicians)
@@ -70,6 +91,21 @@ class Lives(models.Model):
     class Meta:
         unique_together=('ssn','address')
 
+    def __str__(self):
+        return str(self.ssn) + " " + str(self.phone) + " " + str(self.address)
+
+    #print str(self.phone)
+    def clean(self):
+        if (self.address != self.phone.address):
+            raise ValidationError(('Phone and address phone need to be the same.'))
+    def save(self):
+        try:
+            self.full_clean()
+        except ValidationError as e:
+            print e
+
+#https://docs.djangoproject.com/en/1.10/topics/db/queries/
+#https://docs.djangoproject.com/en/1.10/ref/forms/fields/
 
 """
 Customers can search for records by name of the musician, title of the album, and
